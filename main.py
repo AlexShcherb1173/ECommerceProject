ed4b772503c2dd5d3f7b80e1f86c591b08f83740
AlexShcherb1173 <alex181173@list.ru>
2025-08-31 01:31:55 +0300

    new
    # Conflicts:
    #	main.py
    #	main_sky.py

--- main.py
+++ main.py
@@ -1,48 +1,32 @@
 from src.category import Category
-from src.product import LawnGrass, Product, Smartphone
+from src.product import Product
 
 
-if __name__ == '__main__':
-    if __name__ == '__main__':
-        product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-        product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-        product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+p1 = Product("Яблоко", "Красное яблоко", 80, 15)
+p2 = Product("Банан", "Желтый банан", 50, 20)
 
-        print(product1.name)
-        print(product1.description)
-        print(product1.price)
-        print(product1.quantity)
+category = Category("Фрукты", "Свежие фрукты", [p1, p2])
 
-        print(product2.name)
-        print(product2.description)
-        print(product2.price)
-        print(product2.quantity)
+print(category.products)
 
-        print(product3.name)
-        print(product3.description)
-        print(product3.price)
-        print(product3.quantity)
+products = [
+    Product("Яблоко", "Красное яблоко", 80, 15)
+]
+#_______________________________________________________________________________________
+# Создаем новый товар через класс-метод
+new_data = {"name": "Яблоко", "description": "Свежее яблоко", "price": 85, "quantity": 10}
+Product.new_product(new_data, products)
 
-        category1 = Category("Смартфоны",
-                             "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                             [product1, product2, product3])
+# Создаем новый товар, которого нет
+banana_data = {"name": "Банан", "description": "Желтый банан", "price": 50, "quantity": 20}
+Product.new_product(banana_data, products)
 
-        print(category1.name == "Смартфоны")
-        print(category1.description)
-        print(len(category1.products))
-        print(category1.category_count)
-        print(category1.product_count)
+print(products)
+#__________________________________________________________________________________________
+p = Product("Яблоко", "Красное яблоко", 80, 15)
 
-        product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-        category2 = Category("Телевизоры",
-                             "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-                             [product4])
+print(p.price)  # 80
 
-        print(category2.name)
-        print(category2.description)
-        print(len(category2.products))
-        print(category2.products)
-
-        print(Category.category_count)
-        print(Category.product_count)
-    print("Не возникла ошибка TypeError при добавлении не продукта")
+p.price = 0     # ❌ Цена не должна быть нулевая или отрицательная
+p.price = 90    # ✅ Цена повысилась
+p.price = 70    # ⏳ Спросит подтверждение: y/n
